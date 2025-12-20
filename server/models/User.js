448262a22a5ea377a48e8e8fd8bd66e89f071c6d
ShubhasmitMalakar <55288337+ShubhasmitMalakar@users.noreply.github.com>
2025-12-20 15:39:03 +0530

    Login System

--- server/models/User.js
+++ server/models/User.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 const movieSchema = new Schema({
   name: { type: String },
   email: { type: String },
+  otp: { type: String }
 });
 
 mongoose.model("users", movieSchema);
